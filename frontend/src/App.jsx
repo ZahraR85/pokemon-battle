d667667c1d1d60d490954d1a36e988a30013c528
Shabnam <zahra.reihani85@gmail.com>
2024-12-02 11:17:58 +0100

    app.jsx and server.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import PokemonDetails from "./components/Pokemon/PokemonDetails.jsx";
+import RosterPage from "./components/Roster/RosterPage.jsx";
+import BattlePage from "./components/Battle/BattlePage.jsx";
+import LeaderboardPage from "./components/Leaderboard/LeaderboardPage.jsx";
+import LoginForm from "./components/Authentication/LoginForm.jsx";
+import SignupForm from "./components/Authentication/SignupForm.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pokemon/:name" element={<PokemonDetails />} />
+        <Route path="/roster" element={<RosterPage />} />
+        <Route path="/battle" element={<BattlePage />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
