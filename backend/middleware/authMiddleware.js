8693be4c0281f09032ae0b6b5b9286137f388bd7
Shabnam <zahra.reihani85@gmail.com>
2024-12-03 16:08:01 +0100

    authentication2

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,15 +1,22 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+import asyncHandler from 'express-async-handler';
 
-export const protect = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+export const protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-  if (!token) return res.status(401).json({ message: 'Not authorized, no token' });
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(401).json({ message: 'Not authorized, token failed' });
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1]; // Extract token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify token
+      req.user = await User.findById(decoded.id).select('-password'); // Attach user data
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error('Not authorized, token failed');
+    }
+  } else {
+    res.status(401);
+    throw new Error('Not authorized, no token');
   }
-};
+});
